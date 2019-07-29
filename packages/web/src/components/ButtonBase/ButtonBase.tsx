2bb48673fb909e207c47fdf54a4a63536c88b69e
mkinfrared <mkinfrared@gmail.com>
2019-07-29 14:55:22 +0300

    feat(#14): Tests refactoring

--- packages/web/src/components/ButtonBase/ButtonBase.tsx
+++ packages/web/src/components/ButtonBase/ButtonBase.tsx
@@ -3,7 +3,10 @@ import * as React from "react";
 
 import css from "components/ButtonBase/ButtonBase.module.scss";
 
-export const ButtonBase: React.FC<ButtonBaseProps> = ({ buttonText, onClick }) => {
+export const ButtonBase: React.FC<ButtonBaseProps> = ({
+  buttonText,
+  onClick
+}) => {
   return (
     <div className={css.buttonBase}>
       <button onClick={onClick}>{buttonText}</button>
--- packages/web/src/components/Table/Table.test.tsx
+++ packages/web/src/components/Table/Table.test.tsx
@@ -1,11 +1,10 @@
-import * as React from "react";
 import { shallow } from "enzyme";
+import * as React from "react";
 
 import { Table } from "./Table";
 
 describe("Table", () => {
   it("should be defined", () => {
-
     const wrapper = shallow(<Table answers={} user={} deleteAnswer={} />);
   });
 });
